50a9dc26377d2c6406848c2f358f12d23daccc5d
RapidTheNerd <george.marr.cobc@gmail.com>
2017-06-08 22:48:32 +0100

    basic connecting to twitch

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package twitchy
 
+import (
+	"net",
+	"net/textproto",
+	"strings",
+	"bufio"
+)
+
 func main() {
 
+	conn, err := net.Dial("tcp", "irc.chat.twitch.tv:6667")
+		if err != nil {
+			panic(err);
+		}
 }
